49be85627e1919291575ef3b8b8765555dc4db9d
Palak <palak23thakur@gmail.com>
2023-05-30 17:01:50 +0530

    Initial commit_

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './Screens/HomeScreen'
+function App() {
+  return (
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  )
+}
+
+export default App
