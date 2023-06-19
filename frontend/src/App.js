c80db333b01997b365e04111e9433409ad4b3b48
Palak <palak23thakur@gmail.com>
2023-06-19 14:00:19 +0530

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './Screens/HomeScreen'
-function App() {
+const App = () => {
   return (
     <Router>
       <Header />
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,11 +2,14 @@ import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 
 const Footer = () => {
+  const currentYear = new Date().getUTCFullYear()
   return (
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>Copyright &copy;</Col>
+          <Col className='text-center py-3'>
+            <p>ProShop &copy;{currentYear}</p>
+          </Col>
         </Row>
       </Container>
     </footer>
