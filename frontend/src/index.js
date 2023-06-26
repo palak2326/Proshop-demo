76a2b56b6453620c6ccfe3796557da319af1012b
Palak <palak23thakur@gmail.com>
2023-06-27 00:07:44 +0530

    done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,15 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './assets/styles/bootstrap.custom.css'
 import './assets/styles/index.css'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom'
 
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-
+import HomeScreen from './Screens/HomeScreen'
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
+    </Route>
+  )
+)
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
 
